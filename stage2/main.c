b308775c980bfa20e5d413dba2602551252fec33
Daniel Fitz <dfitz360@gmail.com>
2017-03-08 09:49:33 +1000

    Create main file for stage2 and added base includes and methods with comments

---
+++ stage2/main.c
@@ -0,0 +1,32 @@
+/**
+ * *****************************************************************************
+ * @file    stage2/main.c
+ * @author  Daniel Fitzmaurice = 43961229
+ * @date    080317
+ * @brief   Main file for Stage 2
+ * *****************************************************************************
+ */
+
+#include <board.h>
+#include "s4396122_hal_ledbar.h"
+#include "s4396122_hal_pantilt.h"
+
+/**
+ * Initializes the hardware or calls the functions require to initialize the
+ * hardware
+ */
+void Hardware_init() {
+
+}
+
+/**
+ * Main Program Loop
+ * @return  The exit code of the program
+ */
+int main(void) {
+    Hardware_init();
+
+    while (1) { // Main function loop
+
+    }
+}
