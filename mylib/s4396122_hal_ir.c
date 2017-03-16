6667d80c6204f34fbce6b5aa2e17502d22cbd7de
Daniel Fitz <dfitz360@gmail.com>
2017-03-16 18:15:38 +1000

    Added function headers

---
+++ mylib/s4396122_hal_ir.c
@@ -0,0 +1,14 @@
+/**
+ * @file    s4396122_hal_ir.c
+ * @author  Daniel Fitzmaurice = 43961229
+ * @date    150317
+ * @brief   Control library for the IR transmitter and receiver
+ *          REFERENCE: stage3
+ *******************************************************************************
+ * s4396122_hal_ir_init()           Initialise the IR receiver/transmitter
+ *      hardware
+ * irhal_carrier(state)             Internal generic function for turning the
+ *      carrier wave on or off
+ */
+
+#include "s4396122_hal_ir.h"
---
+++ mylib/s4396122_hal_joystick.c
@@ -0,0 +1,13 @@
+/**
+ * @file    s4396122_hal_joystick.c
+ * @author  Daniel Fitzmaurice = 43961229
+ * @date    150317
+ * @brief   Control library for the IR transmitter and receiver
+ *          REFERENCE: stage3
+ *******************************************************************************
+ * s4396122_hal_joystick_init()     Initialise Joystick
+ * int joystick_read(ADC Handler)   Internal Generic function to read specified
+ *      joystick value
+ */
+
+#include "s4396122_hal_joystick.h"
