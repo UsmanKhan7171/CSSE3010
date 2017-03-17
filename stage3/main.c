dc997a9f07c5766ee6b1f54d299a8a6f57d9b168
Daniel Fitz <dfitz360@gmail.com>
2017-03-17 22:27:10 +1000

    Added ir control testing code

--- stage3/main.c
+++ stage3/main.c
@@ -11,6 +11,7 @@
 #include <debug_printf.h>
 #include "s4396122_hal_joystick.h"
 #include "s4396122_hal_ledbar.h"
+#include "s4396122_hal_ir.h"
 
 int map(int test, int oldMin, int oldMax, int newMin, int newMax) {
     return ((test - oldMin) * (newMax - newMin) / (oldMax - oldMin)) + newMin;
@@ -21,6 +22,7 @@ void Hardware_init() {
     BRD_LEDInit();
     s4396122_hal_joystick_init();
     s4396122_hal_ledbar_init();
+    s4396122_hal_ir_init();
 }
 
 int main() {
@@ -28,6 +30,17 @@ int main() {
     Hardware_init();
 
     while (1) {
+
+        if (HAL_GetTick() > 10000) {
+            s4396122_hal_ir_carrieron();
+        } else if (HAL_GetTick() > 5000) {
+            s4396122_hal_ir_carrieroff();
+        }
+
+        s4396122_hal_ir_datamodulation_set();
+        HAL_Delay(250);
+        s4396122_hal_ir_datamodulation_cli();
+        HAL_Delay(250);
 
         int frequency = map(s4396122_hal_joystick_x_read(), 0, 4096, 0, 50);
         int ledBar = 1 << (frequency / 5);
