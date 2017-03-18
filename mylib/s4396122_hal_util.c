f46d954b7d353ada368b9238cc35af20fde89cc1
Daniel Fitz <dfitz360@gmail.com>
2017-03-18 14:54:40 +1000

    Added comments for map function

--- mylib/s4396122_hal_util.c
+++ mylib/s4396122_hal_util.c
@@ -10,6 +10,16 @@
 
 #include "s4396122_hal_util.h"
 
+/**
+ * Linear Maps an integer to a new value using min and max of old variables
+ * (based on the Arduino map function)
+ * @param  test   The integer to be mapped
+ * @param  oldMin The existing minimum value
+ * @param  oldMax The existing maximum value
+ * @param  newMin The new minimum value
+ * @param  newMax The new maximum value
+ * @return        The value that has been mapped
+ */
 int map(int test, int oldMin, int oldMax, int newMin, int newMax) {
     return ((test - oldMin) * (newMax - newMin) / (oldMax - oldMin)) + newMin;
 }
