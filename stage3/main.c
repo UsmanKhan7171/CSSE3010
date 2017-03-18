ab6394bbb98f463968f0ba0328e3c8a4b32ee75c
Daniel Fitz <dfitz360@gmail.com>
2017-03-18 10:16:35 +1000

    Added transmitting code

--- stage3/main.c
+++ stage3/main.c
@@ -29,24 +29,34 @@ int main() {
 
     Hardware_init();
 
+    unsigned int lastTick = HAL_GetTick();
+    int lastState = 0;
+    s4396122_hal_ir_datamodulation_cli();
+
     while (1) {
 
-        if (HAL_GetTick() > 10000) {
-            s4396122_hal_ir_carrieron();
-        } else if (HAL_GetTick() > 5000) {
-            s4396122_hal_ir_carrieroff();
-        }
-
-        s4396122_hal_ir_datamodulation_set();
-        HAL_Delay(250);
-        s4396122_hal_ir_datamodulation_cli();
-        HAL_Delay(250);
+        s4396122_hal_ir_carrieron();
 
         int frequency = map(s4396122_hal_joystick_x_read(), 0, 4096, 0, 50);
         int ledBar = 1 << (frequency / 5);
         s4396122_hal_ledbar_write(ledBar);
 
+        if (frequency >= 5) {
+            int delayPeriod = 1000/frequency;
+            if (HAL_GetTick() > (delayPeriod/2 + lastTick)) {
+                lastTick = HAL_GetTick();
+                if (lastState) {
+                    s4396122_hal_ir_datamodulation_cli();
+                    lastState = 0;
+                } else {
+                    s4396122_hal_ir_datamodulation_set();
+                    lastState = 1;
+                }
+            }
+        } else {
+            lastTick = HAL_GetTick();
+        }
+
         BRD_LEDToggle();
-        HAL_Delay(100);
     }
 }
