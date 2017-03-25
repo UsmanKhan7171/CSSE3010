b463fd0b32754045eb6c5fcaf631088cc3a694a5
Daniel Fitz <dfitz360@gmail.com>
2017-03-25 18:56:33 +1000

    Added function comments

--- mylib/s4396122_util_queue.c
+++ mylib/s4396122_util_queue.c
@@ -1,5 +1,9 @@
 #include "s4396122_util_queue.h"
 
+/**
+ * Creates the queue for reference later on
+ * @return Returns a Queue Object for later applying operations on
+ */
 Queue* s4396122_util_queue_create() {
     Queue *q = malloc(sizeof(Queue));
     q->size = 0;
@@ -8,10 +12,20 @@ Queue* s4396122_util_queue_create() {
     return q;
 }
 
+/**
+ * Gets the size of the Queue
+ * @param  q The Queue for which to read the size from
+ * @return   The size of the Queue
+ */
 int s4396122_util_queue_size(Queue *q) {
     return q->size;
 }
 
+/**
+ * Pushes an element to the top of the Queue
+ * @param q The Queue to push data to
+ * @param d The data to be added
+ */
 void s4396122_util_queue_push(Queue *q, void *d) {
     struct queue *qElement = malloc(sizeof(struct queue));
     qElement->data = d;
@@ -24,6 +38,10 @@ void s4396122_util_queue_push(Queue *q, void *d) {
     q->size++;
 }
 
+/**
+ * Pops an element from the bottom of the Queue and returns the data
+ * @param q Queue to pop from
+ */
 void* s4396122_util_queue_pop(Queue *q) {
     if (q->head == NULL) {
         return NULL;
