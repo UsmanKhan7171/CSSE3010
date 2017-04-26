808e81ef93e8d327fa71b37de7364320a4bb98bf
Daniel Fitz <dfitz360@gmail.com>
2017-04-26 10:28:32 +1000

    Added print libraries

---
+++ mylib/s4396122_util_print.c
@@ -0,0 +1,58 @@
+#include "s4396122_util_print.h"
+
+void s4396122_util_print_move(int x, int y) {
+    debug_printf("\033[%d;%dH", x, y);
+}
+
+void s4396122_util_print_clear() {
+    debug_printf("\033[2J");
+}
+
+void s4396122_util_print_color(int sgr) {
+    debug_printf("\033[%dm", sgr);
+}
+
+void s4396122_util_print_reset() {
+    s4396122_util_print_color(0);
+}
+
+void s4396122_util_print_bold() {
+    s4396122_util_print_color(1);
+}
+
+void s4396122_util_print_reverse() {
+    s4396122_util_print_color(7);
+}
+
+void error(const char *s, const char *fileName, int lineNo) {
+    s4396122_util_print_move(numOfFormatRows + 3, 1);
+    s4396122_util_print_color(s4396122_util_print_red);
+    s4396122_util_print_bold();
+    debug_printf("%s - %s:%d\n", s, fileName, lineNo);
+    s4396122_util_print_reset();
+}
+
+void warn(const char *s, const char *fileName, int lineNo) {
+    s4396122_util_print_move(numOfFormatRows + 3, 1);
+    s4396122_util_print_color(s4396122_util_print_yellow);
+    s4396122_util_print_bold();
+    debug_printf("%s - %s:%d\n", s, fileName, lineNo);
+    s4396122_util_print_reset();
+}
+
+void info(const char *s, const char *fileName, int lineNo) {
+    #ifdef DEBUG
+        s4396122_util_print_move(numOfFormatRows + 3, 1);
+        s4396122_util_print_color(s4396122_util_print_blue);
+        s4396122_util_print_bold();
+        debug_printf("[%s:%d] %s\n", fileName, lineNo, s);
+        s4396122_util_print_reset();
+    #endif
+}
+
+void debug(const char *s, const char *fileName, int lineNo) {
+    #ifdef DEBUG
+        s4396122_util_print_move(numOfFormatRows + 3, 1);
+        debug_printf("[%s:%d] %s\n", fileName, lineNo, s);
+    #endif
+}
---
+++ mylib/s4396122_util_print.h
@@ -0,0 +1,43 @@
+#ifndef UTIL_PRINT_H
+#define UTIL_PRINT_H
+
+#include <debug_printf.h>
+
+#define DEBUG 1
+
+#define s4396122_util_print_black 30
+#define s4396122_util_print_red 31
+#define s4396122_util_print_green 32
+#define s4396122_util_print_yellow 33
+#define s4396122_util_print_blue 34
+#define s4396122_util_print_purple 35
+#define s4396122_util_print_teal 36
+#define s4396122_util_print_white 37
+
+#define s4396122_util_print_black_background 30
+#define s4396122_util_print_red_background 31
+#define s4396122_util_print_green_background 32
+#define s4396122_util_print_yellow_background 33
+#define s4396122_util_print_blue_background 34
+#define s4396122_util_print_purple_background 35
+#define s4396122_util_print_teal_background 36
+#define s4396122_util_print_white_background 37
+
+#define s4396122_util_print_error(s) error(s, __FILE__, __LINE__)
+#define s4396122_util_print_warn(s) warn(s, __FILE__, __LINE__)
+#define s4396122_util_print_info(s) info(s, __FILE__, __LINE__)
+#define s4396122_util_print_debug(s) debug(s, __FILE__, __LINE__)
+
+extern void s4396122_util_print_move(int x, int y);
+extern void s4396122_util_print_clear();
+extern void s4396122_util_print_color(int sgr);
+extern void s4396122_util_print_reset();
+extern void s4396122_util_print_bold();
+extern void s4396122_util_print_reverse();
+
+void error(const char *s, const char *fileName, int lineNo);
+void warn(const char *s, const char *fileName, int lineNo);
+void info(const char *s, const char *fileName, int lineNo);
+void debug(const char *s, const char fileName, int lineNo);
+
+#endif
