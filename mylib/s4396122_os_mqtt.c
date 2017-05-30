270dcc60a4ed37d6bf06fbb5cc0c176bf2d66320
Daniel Fitz <dfitz360@gmail.com>
2017-05-30 16:29:52 +1000

    Got publish working

--- mylib/s4396122_os_mqtt.c
+++ mylib/s4396122_os_mqtt.c
@@ -1,7 +1,36 @@
 #include "s4396122_os_mqtt.h"
 
+Client client;
+
 void s4396122_os_mqtt_init() {
     xTaskCreate(&MQTT_Task, "MQTT", configMINIMAL_STACK_SIZE * 8, NULL, mqttTask_PRIORITY, NULL);
+}
+
+void connect_network(Network *n, char *addr, int port) {
+    int addrSize;
+    /*int sock;*/
+    struct sockaddr_in sAddr;
+
+    /* Create a TCP Socket */
+    if ((n->my_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        debug_printf("can not create socket");
+        return;
+    }
+
+    /*mqtt_socket = n->my_socket;*/
+
+    sAddr.sin_family = AF_INET;
+    sAddr.sin_port = htons((unsigned short) port);
+    sAddr.sin_addr.s_addr = inet_addr(addr);
+
+    if (connect(n->my_socket, (struct sockaddr *)(&sAddr), sizeof (sAddr)) < 0) {
+        debug_printf("MQTT ERROR connecting");
+        return;
+    }
+}
+
+int network_read(Network *n, char *buffer, int len, int timeout) {
+    read(n->my_socket, buffer, len);
 }
 
 void MQTT_Task(void) {
@@ -11,28 +40,34 @@ void MQTT_Task(void) {
     
     Network network;
     NewNetwork(&network);
-    ConnectNetwork(&network, "192.168.0.1", 1883);
+    connect_network(&network, "192.168.0.1", 1883);
     debug_printf("Socket Connected\n");
 
-    Client client;
     MQTTClient(&client, &network, 1000, buff, 100, readBuff, 100);
     debug_printf("Created Client\n");
-    TaskHandle_t taskHandle = xTaskGetHandle("MQTTRD");
-    if (taskHandle == NULL) {
-        debug_printf("Error\n");
-    }
-    vTaskSuspend(taskHandle);
+    client.ipstack->mqttread = &network_read;
     debug_printf("Waiting\n");
-    vTaskDelay(5000);
+    /*vTaskDelay(5000);*/
 
     MQTTConnect(&client, NULL);
     debug_printf("Connected\n");
 
-    while(1);
+    while(1) {
+        /*s4396122_os_mqtt_publish("Test", "Hello");*/
+        vTaskDelay(1000);
+    }
 }
 
 
-void s4396122_os_mqtt_publish(char *topic, char *message) {
+void s4396122_os_mqtt_publish(char *inTopic, char *message) {
+    char topic[50];
+    memset(topic, 0, sizeof(topic));
+    sprintf(topic, "np2/%s", inTopic);
+    MQTTMessage new_message;
+    new_message.payloadlen = strlen(message);
+    new_message.payload = message;
+    new_message.qos = QOS0;
+    MQTTPublish(&client, topic, &new_message);
 }
 
 /**
